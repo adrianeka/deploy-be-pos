447ebbc5501c0a85865521074bc0f43737ceeac7
adrianeka <adrian.eka9@gmail.com>
2025-03-24 19:52:39 +0700

    feat: api CRUD pelanggan

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PelangganController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('pelanggan', PelangganController::class);
+});
